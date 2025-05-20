d891cb491cab40d7bd9cfaa39b61b27ddeb2174b
Gabriel Morales <gabriel18devgmail.com>
2025-05-20 13:33:43 -0300

    first commit

--- portfolio/assets/script.js
+++ portfolio/assets/script.js
@@ -50,6 +50,50 @@ window.addEventListener('resize', function(event) {
     }
 });
 
+// modal //
+const modal = document.getElementById("modal");
+const openBtns = document.querySelectorAll(".open-modal");
+const closeBtn = document.getElementById("close-modal");
+
+openBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    document.body.classList.add("modal-open");
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.add("hidden");
+  document.body.classList.remove("modal-open");
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.add("hidden");
+    document.body.classList.remove("modal-open");
+  }
+});
+
+// fim modal //
+
+// animação modal//
+
+openBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    modal.classList.remove("hidden", "hide");
+    modal.classList.add("show");
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("show");
+  modal.classList.add("hide");
+  setTimeout(() => {
+    modal.classList.add("hidden");
+  }, 300); // espera terminar a animação
+});
+
+// fim animação modal //
 
 //back to top//
 
